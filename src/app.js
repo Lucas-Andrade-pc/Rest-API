bfcd209353773e18aff708c48159b866d4a93537
Lucas-Andrade-pc <84913721+Lucas-Andrade-pc@users.noreply.github.com>
2023-02-18 09:18:44 -0300

    removendo line

--- src/app.js
+++ src/app.js
@@ -2,7 +2,6 @@ const express = require('express');
 const routeProduto = require('./routes/produtos')
 const routPedido = require('./routes/pedido')
 const routeH = require('./routes/health')
-
 const app = express() // instancia do express
 app.disable("x-powered-by");
 
