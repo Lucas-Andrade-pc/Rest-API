92e50350ccec95119a72e252a3d15846f49c200f
Lucas-Andrade-pc <84913721+Lucas-Andrade-pc@users.noreply.github.com>
2023-02-18 09:29:24 -0300

    delete health

--- src/routes/health.js
+++ src/routes/health.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const rota = express.Router();
 
-rota.get('/health', (_req, _res, _next) =>{
+rota.get('/', (_req, _res, _next) =>{
     res.status(200).send({
         message: 'Aplicação Ok'
     })
