671210af3a0bfca7de03878d61aa3a1c4f08b501
Lucas-Andrade-pc <84913721+Lucas-Andrade-pc@users.noreply.github.com>
2023-02-18 09:20:19 -0300

    removendo rotas

--- src/routes/produtos.js
+++ src/routes/produtos.js
@@ -7,26 +7,4 @@ rota.get('/', (_req, _res, _next) =>{
     })
 })
 
-rota.post('/', (_req, _res, _next) =>{
-    res.status(201).send({
-        message: 'Usando post produto'
-    })
-})
-
-rota.delete('/:id_produto', (_req, _res, _next) =>{
-    const id_produto = req.params.id_produto
-    res.status(201).send({
-        message: 'deletando produto',
-        id: id_produto
-    })
-})
-
-rota.patch('/:id_produto', (_req, _res, _next) =>{
-    const id_produto = req.params.id_produto
-    res.status(201).send({
-        message: 'Usando patch produto',
-        id: id_produto
-    })
-})
-
 module.exports = rota
